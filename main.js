5d64a93ae4e2254b5442349804d55d38c67e14f9
jeenlandia <acuarioyeni@hotmail.com>
2023-03-10 00:29:58 -0300

    cambios 09

--- main.js
+++ main.js
@@ -1,19 +1,44 @@
 const operaciones = []  
 
-let categorias = ["Comida", "Educación", "Salidas", "Servicios", "Trabajo", "Transporte"]  
-
 const arrayGanancias = operaciones.filter(operacion => operacion.type === "Ganancia") 
 const totalGanancias = arrayGanancias.reduce((accumulator, currentValue) => accumulator + currentValue.num, 0); 
 
 const arrayGastos = operaciones.filter(operacion => operacion.type === "Gastos") 
 const totalGastos = arrayGastos.reduce((acc, cv) => acc + cv.num, 0);
 
-document.querySelector("#gainings").innerHTML += `<span>${totalGanancias}</span>` 
-document.querySelector("#expenses").innerHTML += `<span>${totalGastos}</span>` 
-document.querySelector("#total").innerHTML += `<span>${totalGanancias - totalGastos}</span>`  
+let categorias = ["Comida", "Educación", "Salidas", "Servicios", "Trabajo", "Transporte"]  
 
 categorias.forEach((categoria) => document.querySelector("#filtro-category").innerHTML += `<option value="">${categoria}</option>`)
 
-// ------------------Nueva operacion------------------------
+document.querySelector("#gainings").innerHTML += `<span>${totalGanancias}</span>` 
+document.querySelector("#expenses").innerHTML += `<span>${totalGastos}</span>` 
+document.querySelector("#total").innerHTML += `<span>${totalGanancias - totalGastos}</span>`
 
-const nuevaOperacion = []
+// boton abrir nueva operacion //
+const $btnViewOperation = document.querySelector("#viewOperation")
+$btnViewOperation.addEventListener("click", () => {
+        $sectionNewOperation.classList.remove("is-hidden")
+        $sectionHome.classList.add("is-hidden")
+});
+
+const $sectionHome = document.querySelector("#balanceHome")
+
+// boton cancelar nueva operacion //
+const $btnViewHome = document.querySelector("#viewHome")
+$btnViewHome.addEventListener("click", () => {
+    $sectionNewOperation.classList.add("is-hidden")
+    $sectionHome.classList.remove("is-hidden")
+});
+
+// boton aceptar nueva operacion //
+const $btnViewHomeAcept = document.querySelector("#viewHomeAcept")
+$btnViewHomeAcept.addEventListener("click", () => {
+    $sectionNewOperation.classList.add("is-hidden")
+    $sectionHome.classList.remove("is-hidden")
+});
+
+// boton categorias //
+
+const $sectionNewOperation = document.querySelector("#sectionNewOperation")     
+const $inputDate = document.querySelector("#inputDate")     
+const $containDate = document.querySelector(".contain-date")
